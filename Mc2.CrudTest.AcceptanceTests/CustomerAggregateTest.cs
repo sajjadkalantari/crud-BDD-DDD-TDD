53ecc5afd6f0a87d3ebbebac2bdee5b22edb8561
sajad <sajadkalantari@outlook.com>
2022-10-17 23:23:59 +0100

    add customer created event with tests

--- Mc2.CrudTest.AcceptanceTests/CustomerAggregateTest.cs
+++ Mc2.CrudTest.AcceptanceTests/CustomerAggregateTest.cs
@@ -1,4 +1,5 @@
 using Mc2.CrudTest.Presentation.Domain.AggregatesModel.CustomerAggregate;
+using Mc2.CrudTest.Presentation.Domain.Events;
 using System;
 using Xunit;
 
@@ -82,6 +83,47 @@ namespace Mc2.CrudTest.AcceptanceTests
 
             //Act - Assert
             Assert.Throws<ArgumentNullException>(() => new Customer(firstname, lastname, dateOfBirth, phoneNumber, email, bankAccountNumber));
+        }
+
+
+        [Fact]
+        public void Add_event_success()
+        {
+            //Arrange
+            var firstname = "fakename";
+            var lastname = "fakeLastname";
+            var dateOfBirth = DateTime.UtcNow;
+            var phoneNumber = "+44 117 496 0123";
+            var email = "test@test.com";
+            var bankAccountNumber = "0000-0000-0000-0000";
+            var expectedResult = 1;
+
+            //Act 
+            var customer = new Customer(firstname, lastname, dateOfBirth, phoneNumber, email, bankAccountNumber);
+            customer.AddDomainEvent(new CustomerCreatedDomainEvent(customer));
+            //Assert
+            Assert.Equal(customer.DomainEvents.Count, expectedResult);
+        }
+
+        [Fact]
+        public void Remove_event_success()
+        {
+            //Arrange
+            var firstname = "fakename";
+            var lastname = "fakeLastname";
+            var dateOfBirth = DateTime.UtcNow;
+            var phoneNumber = "+44 117 496 0123";
+            var email = "test@test.com";
+            var bankAccountNumber = "0000-0000-0000-0000";
+            var expectedResult = 0;
+
+            //Act 
+            var customer = new Customer(firstname, lastname, dateOfBirth, phoneNumber, email, bankAccountNumber);
+            var domainEvent = new CustomerCreatedDomainEvent(customer);
+            customer.AddDomainEvent(domainEvent);
+            customer.RemoveDomainEvent(domainEvent);
+            //Assert
+            Assert.Equal(customer.DomainEvents.Count, expectedResult);
         }
         // Please create more tests based on project requirements as per in readme.md
     }
---
+++ Mc2.CrudTest.Presentation/Mc2.CrudTest.Presentation.Domain/Events/CustomerCreatedDomainEvent.cs
@@ -0,0 +1,20 @@
+﻿using Mc2.CrudTest.Presentation.Domain.AggregatesModel.CustomerAggregate;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mc2.CrudTest.Presentation.Domain.Events
+{
+    public class CustomerCreatedDomainEvent : INotification
+    {
+        public CustomerCreatedDomainEvent(Customer customer)
+        {
+            Customer = customer;
+        }
+        public Customer Customer { get; }
+
+    }
+}
