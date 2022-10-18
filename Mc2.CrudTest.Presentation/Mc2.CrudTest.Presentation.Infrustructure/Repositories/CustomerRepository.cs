7fbe61f2a258b9ec2a43b1bd7baa55c53328bb25
sajad <sajadkalantari@outlook.com>
2022-10-18 07:59:50 +0100

    add customer repository

---
+++ Mc2.CrudTest.Presentation/Mc2.CrudTest.Presentation.Infrustructure/Repositories/CustomerRepository.cs
@@ -0,0 +1,23 @@
+﻿using Mc2.CrudTest.Presentation.Domain.AggregatesModel.CustomerAggregate;
+using Mc2.CrudTest.Presentation.Domain.Infrustructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mc2.CrudTest.Presentation.Infrustructure.Repositories
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        private readonly CustomerContext _context;
+        public IUnitOfWork UnitOfWork => _context;
+
+        public CustomerRepository(CustomerContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+    }
+
+}
