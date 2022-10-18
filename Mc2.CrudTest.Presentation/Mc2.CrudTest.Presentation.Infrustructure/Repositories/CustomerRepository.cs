16a01e9ce24ef0eecf7826d090f03cbcb521b556
sajad <sajadkalantari@outlook.com>
2022-10-18 18:17:15 +0100

    add customer repo methods

--- Mc2.CrudTest.Presentation/Mc2.CrudTest.Presentation.Infrustructure/Repositories/CustomerRepository.cs
+++ Mc2.CrudTest.Presentation/Mc2.CrudTest.Presentation.Infrustructure/Repositories/CustomerRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Mc2.CrudTest.Presentation.Domain.AggregatesModel.CustomerAggregate;
 using Mc2.CrudTest.Presentation.Domain.Infrustructure;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,6 +19,32 @@ namespace Mc2.CrudTest.Presentation.Infrustructure.Repositories
             _context = context ?? throw new ArgumentNullException(nameof(context));
         }
 
+        public Customer Add(Customer customer)
+        {
+            if (customer.IsTransient())
+                return _context.Customers.Add(customer).Entity;
+
+            return customer;
+        }
+
+        public int Delete(Customer customer)
+        {
+            if (!customer.IsTransient())
+                return _context.Customers.Remove(customer).Entity.Id;
+            return customer.Id;
+        }
+
+        public Customer Update(Customer customer)
+        {
+            if (!customer.IsTransient())
+                return _context.Customers.Update(customer).Entity;
+            return customer;
+        }
+
+        public async Task<Customer> FindByIdAsync(int id)
+        {
+            return await _context.Customers.AsNoTracking().Where(b => b.Id == id).SingleOrDefaultAsync();
+        }
     }
 
 }
