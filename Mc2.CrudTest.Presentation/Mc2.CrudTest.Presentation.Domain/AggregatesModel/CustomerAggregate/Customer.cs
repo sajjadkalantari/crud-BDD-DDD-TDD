a9d248ec94b2aaf67c590d0aae1cc8d7ff2521ae
sajad <sajadkalantari@outlook.com>
2022-11-03 11:42:58 +0000

    add value objects

--- Mc2.CrudTest.Presentation/Mc2.CrudTest.Presentation.Domain/AggregatesModel/CustomerAggregate/Customer.cs
+++ Mc2.CrudTest.Presentation/Mc2.CrudTest.Presentation.Domain/AggregatesModel/CustomerAggregate/Customer.cs
@@ -1,82 +1,37 @@
-﻿using IbanNet;
-using Mc2.CrudTest.Presentation.Domain.Infrustructure;
-using Mc2.CrudTest.Shared;
-using Mc2.CrudTest.Shared.Exceptions;
+﻿using Mc2.CrudTest.Presentation.Domain.Infrustructure;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Net.Mail;
 using System.Text;
 using System.Text.RegularExpressions;
 
 namespace Mc2.CrudTest.Presentation.Domain.AggregatesModel.CustomerAggregate
 {
+
     public class Customer : Entity, IAggregateRoot
     {
-        public string Firstname { get; set; }
-        public string Lastname { get; set; }
-        public DateTime DateOfBirth { get; set; }
-        public ulong PhoneNumber { get; set; }
-        public string Email { get; set; }
-        public string BankAccountNumber { get; set; }
+
+        private Customer() { }
 
         public Customer(string firstname, string lastname, DateTime dateOfBirth, string phoneNumber, string email, string bankAccountNumber)
         {
-            Firstname = !string.IsNullOrEmpty(firstname) ? firstname : throw new ArgumentNullException(nameof(firstname));
-            Lastname = !string.IsNullOrEmpty(lastname) ? lastname : throw new ArgumentNullException(nameof(lastname));
+            Firstname = !string.IsNullOrEmpty(firstname) ? firstname.ToLower() : throw new ArgumentNullException(nameof(firstname));
+            Lastname = !string.IsNullOrEmpty(lastname) ? lastname.ToLower() : throw new ArgumentNullException(nameof(lastname));
             DateOfBirth = dateOfBirth;
-            PhoneNumber = ValidatePhonenumber(phoneNumber) ? ParsePhoneNumber(phoneNumber) : throw new CustomSystemException(Constants.ErrorCodes.InvalidMobileNumber, "invalid mobile number");
-            Email = ValidateEmail(email) ? email : throw new CustomSystemException(Constants.ErrorCodes.InvalidEmail, "invalid email address");
-            BankAccountNumber = ValidateBankAccount(bankAccountNumber) ? bankAccountNumber : throw new CustomSystemException(Constants.ErrorCodes.InvalidBankAccount, "Invalid Bank Account Number");
+            PhoneNumber = new PhoneNumberValueObject(phoneNumber);
+            Email = new EmailValueObject(email);
+            BankAccountNumber = new BankAccountNumberValueObject(bankAccountNumber);
         }
         public Customer(int id, string firstname, string lastname, DateTime dateOfBirth, string phoneNumber, string email, string bankAccountNumber) : this(firstname, lastname, dateOfBirth, phoneNumber, email, bankAccountNumber)
         {
             Id = id;
         }
-        private Customer() { }
 
-        private bool ValidatePhonenumber(string phonenumber)
-        {
-            try
-            {
-                if (string.IsNullOrEmpty(phonenumber)) return false;
-                var phoneNumberUtil = PhoneNumbers.PhoneNumberUtil.GetInstance();
-                var phonenumberInstance = phoneNumberUtil.Parse(phonenumber, "IR");
-                return phoneNumberUtil.IsValidNumber(phonenumberInstance);
-            }
-            catch (Exception)
-            {
-                return false;
-            }
-
-        }
-
-        private ulong ParsePhoneNumber(string phonenumber)
-        {
-            var phoneNumberUtil = PhoneNumbers.PhoneNumberUtil.GetInstance();
-            var phonenumberInstance = phoneNumberUtil.Parse(phonenumber, "IR");
-            return phonenumberInstance.NationalNumber;
-        }
-
-        private bool ValidateEmail(string email)
-        {
-            try
-            {
-                var mail = new MailAddress(email);
-                return true;
-            }
-            catch (FormatException)
-            {
-                return false;
-            }
-        }
-
-        private bool ValidateBankAccount(string bankAccount)
-        {
-            IIbanValidator validator = new IbanValidator();
-            ValidationResult validationResult = validator.Validate(bankAccount);
-            return validationResult.IsValid;
-        }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public PhoneNumberValueObject PhoneNumber { get; set; }
+        public EmailValueObject Email { get; set; }
+        public BankAccountNumberValueObject BankAccountNumber { get; set; }
 
     }
 }
---
+++ Mc2.CrudTest.Presentation/Mc2.CrudTest.Presentation.Domain/AggregatesModel/CustomerAggregate/PhoneNumberValueObject.cs
@@ -0,0 +1,55 @@
+﻿using Mc2.CrudTest.Presentation.Domain.Infrustructure;
+using Mc2.CrudTest.Shared;
+using Mc2.CrudTest.Shared.Exceptions;
+using System;
+using System.Collections.Generic;
+
+namespace Mc2.CrudTest.Presentation.Domain.AggregatesModel.CustomerAggregate
+{
+    public class PhoneNumberValueObject: ValueObject
+    {
+        public PhoneNumberValueObject()
+        {
+
+        }
+        public PhoneNumberValueObject(string phoneNumber)
+        {
+            PhoneNumber = ValidatePhonenumber(phoneNumber) ? ParsePhoneNumber(phoneNumber) : throw new CustomSystemException(Constants.ErrorCodes.InvalidMobileNumber, "invalid mobile number");
+        }
+
+        public ulong PhoneNumber { get; set; }
+
+        private bool ValidatePhonenumber(string phonenumber)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(phonenumber)) return false;
+                var phoneNumberUtil = PhoneNumbers.PhoneNumberUtil.GetInstance();
+                var phonenumberInstance = phoneNumberUtil.Parse(phonenumber, "IR");
+                return phoneNumberUtil.IsValidNumber(phonenumberInstance);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+        }
+
+        private ulong ParsePhoneNumber(string phonenumber)
+        {
+            var phoneNumberUtil = PhoneNumbers.PhoneNumberUtil.GetInstance();
+            var phonenumberInstance = phoneNumberUtil.Parse(phonenumber, "IR");
+            return phonenumberInstance.NationalNumber;
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return PhoneNumber;
+        }
+
+        public override string ToString()
+        {
+            return this.PhoneNumber.ToString();
+        }
+    }
+}
