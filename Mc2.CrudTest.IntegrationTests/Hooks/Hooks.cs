d33bb636a0a7c2bfb85fe5bb392624e4a6ff4f27
sajad <sajadkalantari@outlook.com>
2022-11-03 04:49:43 +0000

    clear database after each test

--- Mc2.CrudTest.IntegrationTests/Hooks/Hooks.cs
+++ Mc2.CrudTest.IntegrationTests/Hooks/Hooks.cs
@@ -2,6 +2,8 @@
 using Microsoft.Extensions.Hosting;
 using TechTalk.SpecFlow;
 using Mc2.CrudTest.Presentation.Server;
+using Mc2.CrudTest.Presentation.Infrustructure;
+using Microsoft.EntityFrameworkCore;
 
 namespace Mc2.CrudTest.IntegrationTests.Hooks
 {
@@ -14,13 +16,16 @@ namespace Mc2.CrudTest.IntegrationTests.Hooks
         public static void BeforeTestRun()
         {
             _host = Program.CreateHostBuilder(null).Build();
-
             _host.Start();
         }
 
         [AfterTestRun]
         public static void AfterTestRun()
         {
+            var ctx = (CustomerContext) _host.Services.GetService(typeof(CustomerContext));
+            var customers = ctx.Customers.ToListAsync().GetAwaiter().GetResult();
+            ctx.RemoveRange(customers);
+            ctx.SaveChanges();
             _host.StopAsync().Wait();
         }
     }
