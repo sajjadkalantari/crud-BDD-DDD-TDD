70c901b4d8e69192a1bde0601d6dae297f19e68f
sajad <sajadkalantari@outlook.com>
2022-11-03 04:53:15 +0000

    add response dto

---
+++ Mc2.CrudTest.Presentation/Mc2.CrudTest.Presentation.Application/Dtos/ResponseDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Mc2.CrudTest.Presentation.Application.Dtos
+{
+    public class ResponseDTO
+    {
+        public bool Succeeded { get; set; }
+        public int Code { get; set; }
+        public string Description { get; set; }
+    }
+}
